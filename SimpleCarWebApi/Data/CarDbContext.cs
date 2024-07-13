bfeec27fb371e72c97d2359f4570041eb73c5955
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-07-13 18:52:15 +0200

    Add CarDbContext

---
+++ SimpleCarWebApi/Data/CarDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleCarWebApi.Models;
+
+namespace SimpleCarWebApi.Data
+{
+    public class CarDbContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarBrand> CarBrands { get; set; }
+        public DbSet<CarBodyType> CarBodyTypes { get; set; }
+
+        public CarDbContext(DbContextOptions<CarDbContext> options) : base(options)
+        {
+        }
+    }
+}
